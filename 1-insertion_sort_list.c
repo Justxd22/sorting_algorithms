140933d108a30eaf349fa5d3d631d754ff4a233c
Justxd22 <xdjust18@gmail.com>
2023-12-20 16:30:55 +0200

    iiiii

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Insertion sort algo.
+ *
+ * @list: Linked list to sort
+ * No Return
+ */
+void insertion_sort_list(listint_t **list)
+{
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	listint_t *curr, *temp;
+
+	curr = (*list)->next;
+
+	while (curr != NULL)
+	{
+		temp = curr->prev;
+		while (temp != NULL && temp->n > curr->n)
+		{
+			temp->next = curr->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp;
+
+			curr->prev = temp->prev;
+			if (curr->prev != NULL)
+				curr->prev->next = curr;
+
+			temp->prev = curr;
+			curr->next = temp;
+
+			if (curr->prev == NULL)
+				*list = curr;
+
+			print_list(*list);
+			temp = curr->prev;
+		}
+		curr = curr->next;
+	}
+}
